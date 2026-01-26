30a83cc8234e03e39c7b59328bcf10f0b82d8dfc
unknown <ayman507032@gmail.com>
2026-01-26 15:43:26 +0530

    achanged db,js and gitignore for deployinh

--- backend/database/db.js
+++ backend/database/db.js
@@ -5,8 +5,16 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const db = new sqlite3.Database(
-  path.join(__dirname, "app.db")
-);
+const dbPath = path.join(__dirname, "../database/app.db");
+
+console.log("Using SQLite DB at:", dbPath);
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error("❌ Failed to connect to SQLite:", err.message);
+  } else {
+    console.log("✅ Connected to SQLite database");
+  }
+});
 
 export default db;
